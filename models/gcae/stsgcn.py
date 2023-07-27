973d318f765d487cbece97d329a598f2fb263188
stdrr <development.stdrr@gmail.com>
2023-07-27 22:59:10 +0200

    Merge classes and refactor.
    Changelog:
    - removed stsgcn_diffusion_unet.py
    - merged stsgcn_diffusion_unet.py and stsgcn.py
    - refactored code
    - added docstrings

--- models/gcae/stsgcn.py
+++ models/gcae/stsgcn.py
@@ -1,258 +1,199 @@
-#!/usr/bin/env python
-# coding: utf-8
-
+import math
+from typing import List, Tuple, Union
 
 import torch
 import torch.nn as nn
-import math
-
-
-
-
-class ConvTemporalGraphical(nn.Module):
-    #Source : https://github.com/yysijie/st-gcn/blob/master/net/st_gcn.py
-    r"""The basic module for applying a graph convolution.
-    Args:
-        in_channels (int): Number of channels in the input sequence data
-        out_channels (int): Number of channels produced by the convolution
-        kernel_size (int): Size of the graph convolving kernel
-        t_kernel_size (int): Size of the temporal convolving kernel
-        t_stride (int, optional): Stride of the temporal convolution. Default: 1
-        t_padding (int, optional): Temporal zero-padding added to both sides of
-            the input. Default: 0
-        t_dilation (int, optional): Spacing between temporal kernel elements.
-            Default: 1
-        bias (bool, optional): If ``True``, adds a learnable bias to the output.
-            Default: ``True``
-    Shape:
-        - Input: Input graph sequence in :math:`(N, in_channels, T_{in}, V)` format
-        - Output: Outpu graph sequence in :math:`(N, out_channels, T_{out}, V)` format
-        where
-            :math:`N` is a batch size,
-            :math:`K` is the spatial kernel size, as :math:`K == kernel_size[1]`,
-            :math:`T_{in}/T_{out}` is a length of input/output sequence,
-            :math:`V` is the number of graph nodes. 
-    """
-    def __init__(self,
-                 time_dim,
-                 joints_dim
-    ):
-        super(ConvTemporalGraphical,self).__init__()
-        
-        self.A=nn.Parameter(torch.FloatTensor(time_dim, joints_dim,joints_dim)) #learnable, graph-agnostic 3-d adjacency matrix(or edge importance matrix)
-        stdv = 1. / math.sqrt(self.A.size(1))
-        self.A.data.uniform_(-stdv,stdv)
-
-        self.T=nn.Parameter(torch.FloatTensor(joints_dim , time_dim, time_dim)) 
-        stdv = 1. / math.sqrt(self.T.size(1))
-        self.T.data.uniform_(-stdv,stdv)
-        '''
-        self.prelu = nn.PReLU()
-        
-        self.Z=nn.Parameter(torch.FloatTensor(joints_dim, joints_dim, time_dim, time_dim)) 
-        stdv = 1. / math.sqrt(self.Z.size(2))
-        self.Z.data.uniform_(-stdv,stdv)
-        '''
-    def forward(self, x):
-        x = torch.einsum('nctv,vtq->ncqv', (x, self.T))
-        ## x=self.prelu(x)
-        x = torch.einsum('nctv,tvw->nctw', (x, self.A))
-        ## x = torch.einsum('nctv,wvtq->ncqw', (x, self.Z))
-        return x.contiguous() 
-
 
 
 
 class ST_GCNN_layer(nn.Module):
-    """
-    Shape:
-        - Input[0]: Input graph sequence in :math:`(N, in_channels, T_{in}, V)` format
-        - Input[1]: Input graph adjacency matrix in :math:`(K, V, V)` format
-        - Output[0]: Outpu graph sequence in :math:`(N, out_channels, T_{out}, V)` format
-        where
-            :math:`N` is a batch size,
-            :math:`K` is the spatial kernel size, as :math:`K == kernel_size[1]`,
-            :math:`T_{in}/T_{out}` is a length of input/output sequence,
-            :math:`V` is the number of graph nodes. 
-            :in_channels= dimension of coordinates
-            : out_channels=dimension of coordinates
-            +
-    """
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 kernel_size,
-                 stride,
-                 time_dim,
-                 joints_dim,
-                 dropout,
-                 bias=True):
+    
+    def __init__(self, in_channels:int, out_channels:int, kernel_size:Union[Tuple[int], List[int]],
+                 stride:int, time_dim:int, joints_dim:int, dropout:float, bias:bool=True, emb_dim:int=None) -> None:
+        """
+        Space-Time-Seperable Graph Convolutional Layer.
+
+        Args:
+            in_channels (int): input channels
+            out_channels (int): output channels
+            kernel_size (Union[Tuple[int], List[int]]): kernel size of the convolutional layer
+            stride (int): stride of the convolutional layer
+            time_dim (int): time dimension
+            joints_dim (int): joints dimension
+            dropout (float): dropout probability
+            bias (bool, optional): whether to use bias in the convolutional layers. Defaults to True.
+            emb_dim (int, optional): embedding dimension. Defaults to None.
+        """
         
         super(ST_GCNN_layer,self).__init__()
+        
+        # Set the model's parameters
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        self.time_dim = time_dim
+        self.joints_dim = joints_dim
+        self.dropout = dropout
+        self.bias = bias
+        self.emb_dim = emb_dim
         self.kernel_size = kernel_size
         assert self.kernel_size[0] % 2 == 1
         assert self.kernel_size[1] % 2 == 1
+        
+        # Build the model
+        self.build_model()
+
+    
+    def build_model(self) -> None:
+        """
+        Build the model.
+        """
+        
         padding = ((self.kernel_size[0] - 1) // 2,(self.kernel_size[1] - 1) // 2)
         
-        
-        self.gcn=ConvTemporalGraphical(time_dim,joints_dim) # the convolution layer
+        self.gcn=ConvTemporalGraphical(self.time_dim, self.joints_dim)
         
         self.tcn = nn.Sequential(
             nn.Conv2d(
-                in_channels,
-                out_channels,
+                self.in_channels,
+                self.out_channels,
                 (self.kernel_size[0], self.kernel_size[1]),
-                (stride, stride),
+                (self.stride, self.stride),
                 padding,
+                bias=self.bias
             ),
-            nn.BatchNorm2d(out_channels),
-            nn.Dropout(dropout, inplace=True),
+            nn.BatchNorm2d(self.out_channels),
+            nn.Dropout(self.dropout, inplace=True),
         )
-
-
-            
         
-                
-        
-        if stride != 1 or in_channels != out_channels: 
+        if self.stride != 1 or self.in_channels != self.out_channels: 
 
-            self.residual=nn.Sequential(nn.Conv2d(
-                    in_channels,
-                    out_channels,
-                    kernel_size=1,
-                    stride=(1, 1)),
-                nn.BatchNorm2d(out_channels),
-            )
-            
+            self.residual = nn.Sequential(
+                            nn.Conv2d(self.in_channels,
+                                      self.out_channels,
+                                      kernel_size=1,
+                                      stride=(1, 1),
+                                      bias=self.bias),
+                            nn.BatchNorm2d(self.out_channels))   
             
         else:
             self.residual=nn.Identity()
-        
-        
+    
         self.prelu = nn.PReLU()
 
+        if self.emb_dim is not None:
+            self.emb_layer = nn.Sequential(
+                nn.SiLU(),
+                nn.Linear(
+                    self.emb_dim,
+                    self.out_channels
+                ),
+            )
         
 
-    def forward(self, x):
-     #   assert A.shape[0] == self.kernel_size[1], print(A.shape[0],self.kernel_size)
-        res=self.residual(x)
-        x=self.gcn(x) 
-        x=self.tcn(x)
-        x=x+res
-        x=self.prelu(x)
-        return x
+    def forward(self, X:torch.Tensor, t:torch.Tensor=None) -> torch.Tensor:
+        """
+        Forward pass of the model.
+
+        Args:
+            X (torch.Tensor): input tensor of shape [batch_size, in_channels, time_dim, joints_dim]
+            t (torch.Tensor, optional): time tensor of shape [batch_size, time_embedding_dim]. Defaults to None.
+
+        Returns:
+            torch.Tensor: output tensor of shape [batch_size, out_channels, time_dim, joints_dim]
+        """
+        
+        res = self.residual(X)
+        X = self.gcn(X) 
+        X = self.tcn(X)
+        X = X + res
+        X = self.prelu(X)
+        
+        if (self.emb_dim is not None) and (t is not None):
+            emb = self.emb_layer(t)[:, :, None, None].repeat(1, 1, X.shape[-2], X.shape[-1])
+            return X + emb
+        else:
+            return X
+
+
+
+class ConvTemporalGraphical(nn.Module):
+    
+    def __init__(self, time_dim:int, joints_dim:int) -> None:
+        """
+        The basic module for applying a graph convolution.
+        Source: https://github.com/yysijie/st-gcn/blob/master/net/st_gcn.py
+        
+        Args:
+            time_dim (int): number of frames
+            joints_dim (int): number of joints
+        """
+        
+        super(ConvTemporalGraphical,self).__init__()
+        
+        self.A=nn.Parameter(torch.FloatTensor(time_dim, joints_dim, joints_dim)) #learnable, graph-agnostic 3-d adjacency matrix(or edge importance matrix)
+        stdv = 1. / math.sqrt(self.A.size(1))
+        self.A.data.uniform_(-stdv, stdv)
+
+        self.T=nn.Parameter(torch.FloatTensor(joints_dim, time_dim, time_dim)) 
+        stdv = 1. / math.sqrt(self.T.size(1))
+        self.T.data.uniform_(-stdv, stdv)
+        
+        
+    def forward(self, X:torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the model.
+
+        Args:
+            X (torch.Tensor): input tensor of shape [batch_size, in_channels, time_dim, joints_dim]
+
+        Returns:
+            torch.Tensor: output tensor of shape [batch_size, in_channels, time_dim, joints_dim]
+        """
+        
+        X = torch.einsum('nctv,vtq->ncqv', (X, self.T))
+        X = torch.einsum('nctv,tvw->nctw', (X, self.A))
+        return X.contiguous() 
 
 
 
 
-class CNN_layer(nn.Module): # This is the simple CNN layer,that performs a 2-D convolution while maintaining the dimensions of the input(except for the features dimension)
+class CNN_layer(nn.Module):
 
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 kernel_size,
-                 dropout,
-                 bias=True):
+    def __init__(self, in_channels:int, out_channels:int, kernel_size:Union[Tuple[int], List[int]],
+                 dropout:float, bias=True) -> None:
+        """
+        This is the simple CNN layer that performs a 2-D convolution while maintaining the dimensions of the input (except for the features dimension).
+
+        Args:
+            in_channels (int): number of channels of the input
+            out_channels (int): number of channels of the output
+            kernel_size (Union[Tuple[int], List[int]]): kernel size of the convolution
+            dropout (float): dropout probability
+            bias (bool, optional): whether to use bias in the convolutional layers. Defaults to True.
+        """
         
         super(CNN_layer,self).__init__()
         self.kernel_size = kernel_size
         padding = ((kernel_size[0] - 1) // 2, (kernel_size[1] - 1) // 2) # padding so that both dimensions are maintained
         assert kernel_size[0] % 2 == 1 and kernel_size[1] % 2 == 1
 
-        
-        
-        self.block= [nn.Conv2d(in_channels,out_channels,kernel_size=kernel_size,padding=padding)
-                     ,nn.BatchNorm2d(out_channels),nn.Dropout(dropout, inplace=True)] 
+        self.block= [nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding, bias=bias),
+                     nn.BatchNorm2d(out_channels), nn.Dropout(dropout, inplace=True)] 
 
-
-
-            
-        
         self.block=nn.Sequential(*self.block)
         
 
-    def forward(self, x):
+    def forward(self, X:torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the model.
+
+        Args:
+            X (torch.Tensor): input tensor of shape [batch_size, in_channels, time_dim, joints_dim]
+
+        Returns:
+            torch.Tensor: output tensor of shape [batch_size, out_channels, time_dim, joints_dim]
+        """
         
-        output= self.block(x)
+        output= self.block(X)
         return output
-
-
-# In[11]:
-
-
-class Model(nn.Module):
-    """ 
-    Shape:
-        - Input[0]: Input sequence in :math:`(N, in_channels,T_in, V)` format
-        - Output[0]: Output sequence in :math:`(N,T_out,in_channels, V)` format
-        where
-            :math:`N` is a batch size,
-            :math:`T_{in}/T_{out}` is a length of input/output sequence,
-            :math:`V` is the number of graph nodes. 
-            :in_channels=number of channels for the coordiantes(default=3)
-            +
-    """
-
-    def __init__(self,
-                 input_channels,
-                 input_time_frame,
-                 output_time_frame,
-                 st_gcnn_dropout,
-                 joints_to_consider,
-                 n_txcnn_layers,
-                 txc_kernel_size,
-                 txc_dropout,
-                 bias=True):
-        
-        super(Model,self).__init__()
-        self.input_time_frame=input_time_frame
-        self.output_time_frame=output_time_frame
-        self.joints_to_consider=joints_to_consider
-        self.st_gcnns=nn.ModuleList()
-        self.n_txcnn_layers=n_txcnn_layers
-        self.txcnns=nn.ModuleList()
-        
-      
-        self.st_gcnns.append(ST_GCNN_layer(input_channels,64,[1,1],1,input_time_frame,
-                                           joints_to_consider,st_gcnn_dropout))
-        self.st_gcnns.append(ST_GCNN_layer(64,32,[1,1],1,input_time_frame,
-                                               joints_to_consider,st_gcnn_dropout))
-            
-        self.st_gcnns.append(ST_GCNN_layer(32,64,[1,1],1,input_time_frame,
-                                               joints_to_consider,st_gcnn_dropout))
-                                               
-        self.st_gcnns.append(ST_GCNN_layer(64,input_channels,[1,1],1,input_time_frame,
-                                               joints_to_consider,st_gcnn_dropout))                                               
-                
-                
-                # at this point, we must permute the dimensions of the gcn network, from (N,C,T,V) into (N,T,C,V)           
-        self.txcnns.append(CNN_layer(input_time_frame,output_time_frame,txc_kernel_size,txc_dropout)) # with kernel_size[3,3] the dimensinons of C,V will be maintained       
-        for i in range(1,n_txcnn_layers):
-            self.txcnns.append(CNN_layer(output_time_frame,output_time_frame,txc_kernel_size,txc_dropout))
-        
-            
-        self.prelus = nn.ModuleList()
-        for j in range(n_txcnn_layers):
-            self.prelus.append(nn.PReLU())
-
-
-        
-
-    def forward(self, x):
-        for gcn in (self.st_gcnns):
-            x = gcn(x)
-            
-        x= x.permute(0,2,1,3) # prepare the input for the Time-Extrapolator-CNN (NCTV->NTCV)
-        
-        x=self.prelus[0](self.txcnns[0](x))
-        
-        for i in range(1,self.n_txcnn_layers):
-            x = self.prelus[i](self.txcnns[i](x)) +x # residual connection
-            
-        return x
-        
-
-
-
-
-
