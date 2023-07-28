9271f63fd1fb7f2e79d597000925cdc89acc50ac
stdrr <development.stdrr@gmail.com>
2023-07-28 18:01:48 +0200

    Remove unused functions and add dataset masks.
    Changelog:
    - added functions to get the HR masks for Avenue and UBnormal
    - removed `windows_based_loss(..)`
    - removed `windows_based_loss_diffusion(..)`
    - removed `windows_based_loss_rec_and_hy(..)`
    - removed `windows_based_loss_mahalanobis(..)`
    - removed `mahalanobis(..)`
    - removed unused imports
    - removed commented code

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -1,15 +1,13 @@
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-import torch
-from sklearn.metrics import roc_curve,roc_auc_score
-import matplotlib.pyplot as plt
-import torch.nn as nn
-import warnings
-from scipy.ndimage import gaussian_filter1d
-import geoopt.manifolds.stereographic.math as gmath
-import pandas as pd
-warnings.filterwarnings("ignore")
+import os
+from glob import glob
 
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from scipy.ndimage import gaussian_filter1d
+from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.preprocessing import MinMaxScaler
+    
 
 def compute_fig_matrix(pos, frames_pos, n_frames):
     assert len(pos.shape) == 4
@@ -20,122 +18,20 @@ def compute_fig_matrix(pos, frames_pos, n_frames):
     pose = np.zeros(shape=(w, n_frames, joints*dim))
 
     for n in range(pose.shape[0]):
-        pose[n, frames_pos[n] - 1, :] = pos[n, :, :] # added -1
+        pose[n, frames_pos[n] - 1, :] = pos[n, :, :]
     
     return pose
+
 
 def compute_var_matrix(pos, frames_pos, n_frames):
 
     pose = np.zeros(shape=(pos.shape[0], n_frames))
 
     for n in range(pose.shape[0]):
-        pose[n, frames_pos[n] - 1] = pos[n] # added -1
+        pose[n, frames_pos[n] - 1] = pos[n]
 
     return pose
 
-
-def mahalanobis(u:torch.tensor, v:torch.tensor, VI:torch.tensor, reduce='mean'):
-    if len(u.size()) < 3:
-        u = torch.reshape(u, (*u.size(), 1))
-    if len(v.size()) < 3:
-        v = torch.reshape(v, (*v.size(), 1))
-    distance = torch.sqrt(torch.matmul(torch.matmul(torch.transpose(u - v, 1, 2), VI), u - v))
-    
-    if reduce == 'mean':
-        return distance.mean()
-    else:
-        return distance
-    
-    
-def windows_based_loss_mahalanobis(hidden_c, hidden_out_fig, VI, frames_fig, n_frames): # VI is the inverse covariance matrix
-    
-    w = hidden_out_fig.shape[0]
-    
-    hidden_out_fig = torch.from_numpy(hidden_out_fig).cuda()
-    loss_hypersphere = mahalanobis(hidden_out_fig, hidden_c, VI, reduce='none')
-    loss_hypersphere = torch.mean(loss_hypersphere, dim=-1)
-
-    pose = np.zeros(shape=(w, n_frames))
-
-    for n in range(pose.shape[0]):
-        pose[n, frames_fig[n] - 1] = loss_hypersphere[n].cpu() # added -1
-
-    return pose
-
-
-
-
-def windows_based_loss_rec_and_hy(gt_fig, out_fig, hidden_c, hidden_out_fig, frames_fig, n_frames, loss_fn=nn.MSELoss(reduction='none'), rec_loss_weight=0.2, loss_type='rec'):
-    assert len(gt_fig.shape) == 4
-    w, dim, timesteps, joints = gt_fig.shape
-
-    gt_fig = gt_fig.transpose(0, 2, 3, 1).reshape(-1, timesteps*joints*dim)
-    out_fig = out_fig.transpose(0, 2, 3, 1).reshape(-1, timesteps*joints*dim)
-
-    gt_fig = torch.from_numpy(gt_fig).cuda()
-    out_fig = torch.from_numpy(out_fig).cuda()
-    loss = loss_fn(gt_fig,out_fig)
-    loss = torch.mean(loss, dim=-1)
-    
-    hidden_out_fig = torch.from_numpy(hidden_out_fig).cuda()
-    loss_hypersphere = loss_fn(hidden_c,hidden_out_fig)
-    loss_hypersphere = torch.mean(loss_hypersphere, dim=-1)
-    if loss_type == 'rec+hyp':
-        loss /= rec_loss_weight     
-    # print(torch.mean(loss),torch.mean(loss_hypersphere))
-    pose = np.zeros(shape=(w, n_frames))
-
-    for n in range(pose.shape[0]):
-        # pose[n, frames_fig[n] - 1] = rec_loss_weight*loss[n].cpu() + loss_hypersphere[n].cpu() # added -1
-        if loss_type == 'rec':
-            pose[n, frames_fig[n] - 1] = loss[n].cpu()
-        if loss_type == 'hyp':
-            pose[n, frames_fig[n] - 1] = loss_hypersphere[n].cpu()
-        if loss_type == 'rec+hyp':
-            pose[n, frames_fig[n] - 1] = loss[n].cpu() + loss_hypersphere[n].cpu()
-            
-    return pose
-
-
-def windows_based_loss(gt_fig, out_fig, frames_fig, n_frames, loss_fn=nn.MSELoss(reduction="none")):
-    assert len(gt_fig.shape) == 4
-    w, dim, timesteps, joints = gt_fig.shape
-
-    gt_fig = gt_fig.transpose(0, 2, 3, 1).reshape(-1, timesteps*joints*dim)
-    out_fig = out_fig.transpose(0, 2, 3, 1).reshape(-1, timesteps*joints*dim)
-
-    # loss_fn = nn.MSELoss(reduction="none")
-    gt_fig = torch.from_numpy(gt_fig).cuda()
-    out_fig = torch.from_numpy(out_fig).cuda()
-    loss = loss_fn(gt_fig,out_fig)
-    loss = torch.mean(loss, dim=-1)
-
-    pose = np.zeros(shape=(w, n_frames))
-
-    for n in range(pose.shape[0]):
-        pose[n, frames_fig[n] - 1] = loss[n].cpu() # added -1
-
-    return pose
- 
-def windows_based_loss_diffusion(gt_fig, out_fig, frames_fig, n_frames, loss_fn=nn.MSELoss(reduction="none")):
-    assert len(gt_fig.shape) == 4
-    w, dim, timesteps, joints = gt_fig.shape
-
-    gt_fig = gt_fig.transpose(0, 2, 3, 1).reshape(-1, timesteps*joints*dim)
-    out_fig = out_fig.transpose(0, 2, 3, 1).reshape(-1, timesteps*joints*dim)
-
-    # loss_fn = nn.MSELoss(reduction="none")
-    gt_fig = torch.from_numpy(gt_fig).cuda()
-    out_fig = torch.from_numpy(out_fig).cuda()
-    loss = loss_fn(gt_fig,out_fig)
-    loss = torch.mean(loss, dim=-1)
-
-    pose = np.zeros(shape=(w, n_frames))
-
-    for n in range(pose.shape[0]):
-        pose[n, frames_fig[n] - 1] = loss[n].cpu() # added -1
-
-    return pose
 
 def extract_single_pose(pose_matrix, method="median", std=False, std_method="mean", std_lift=1e-7):
     single_pose = np.zeros((pose_matrix.shape[1], pose_matrix.shape[2]))
@@ -174,11 +70,14 @@ def extract_single_pose(pose_matrix, method="median", std=False, std_method="mea
         return single_pose, std_score
     return single_pose, []
 
+
 def filter_vectors_by_cond(vecs, cond):
     return [filter_by_cond(vec, cond) for vec in vecs]
 
+
 def filter_by_cond(vec, cond):
     return vec[cond]
+
 
 def calculate_loss(loss_func, input, target, dataname, to_pow=False):
     if 'array' in str(type(input)):    
@@ -197,33 +96,12 @@ def calculate_loss(loss_func, input, target, dataname, to_pow=False):
     return reco_loss.detach().cpu().numpy()
 
 
-# def score_process(score, win_size=50, dataname='STC', use_scaler = False):
+def score_process(score):
     
-#     score = pd.Series(score).rolling(win_size, center=True, min_periods=win_size//2).mean().values
-#     if use_scaler:
-#         scaler = MinMaxScaler()
-#         score = scaler.fit_transform(score.reshape(-1, 1)).reshape(-1)
-#     return score
-
-
-def score_process(score, win_size=50, dataname='STC', use_scaler = False):
-    
-    # score = pd.Series(score).rolling(win_size, center=True, min_periods=win_size//2).mean().values
-    # # if use_scaler:
-    # scaler = MinMaxScaler()
-    # score = scaler.fit_transform(score.reshape(-1, 1)).reshape(-1)
-        
     scores_shifted = np.zeros_like(score)
     shift = 8 + (8 // 2) - 1
     scores_shifted[shift:] = score[:-shift]
     score = gaussian_filter1d(scores_shifted, 30)
-    
-    #### STC
-    # scores_shifted = np.zeros_like(score)
-    # shift = 6 + (6 // 2) - 1
-    # scores_shifted[shift:] = score[:-shift]
-    # score = gaussian_filter1d(scores_shifted, 10)   
-    
     
     return score
 
@@ -233,6 +111,7 @@ def ranges(nums):
     gaps = [[s, e] for s, e in zip(nums, nums[1:]) if s+1 < e]
     edges = iter(nums[:1] + sum(gaps, []) + nums[-1:])
     return list(zip(edges, edges))
+
 
 def ROC(y_test,y_pred, path=''):
     fpr,tpr,tr=roc_curve(y_test,y_pred)
@@ -268,3 +147,33 @@ def pad_scores(fig_reconstruction_loss, gt, pad_size):
         for interval in nope: 
             fig_reconstruction_loss[range(interval[0],interval[1])] = 0
         return fig_reconstruction_loss
+    
+    
+def get_avenue_mask():
+    V_01 = [1] * 75 + [0] * 46 + [1] * 269 + [0] * 47 + [1] * 427 + [0] * 47 + [1] * 20 + [0] * 70 + [1] * 438  # 1439 Frames
+    V_02 = [1] * 272 + [0] * 48 + [1] * 403 + [0] * 41 + [1] * 447  # 1211 Frames
+    V_03 = [1] * 293 + [0] * 48 + [1] * 582  # 923 Frames
+    V_06 = [1] * 561 + [0] * 64 + [1] * 189 + [0] * 193 + [1] * 276  # 1283 Frames
+    V_16 = [1] * 728 + [0] * 12  # 740 Frames
+
+    masked_clips = {
+        1: V_01,
+        2: V_02,
+        3: V_03,
+        6: V_06,
+        16: V_16
+    }
+    return masked_clips
+
+
+def get_hr_ubnormal_mask(path_to_boolean_masks):
+    
+    hr_boolean_masks = glob(path_to_boolean_masks)
+    hr_ubnormal_masked_clips = {}
+    
+    for boolean_mask_path in hr_boolean_masks:
+        scene_clip_id = os.path.basename(boolean_mask_path).split('.')[0]
+        scene_id, clip_id = list(map(int, scene_clip_id.split('_')))
+        hr_ubnormal_masked_clips[(scene_id, clip_id)] = np.load(boolean_mask_path)
+    
+    return hr_ubnormal_masked_clips
