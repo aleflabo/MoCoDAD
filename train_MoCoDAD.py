98f167840a522b442fef3ee167bc8f69044fa677
stdrr <development.stdrr@gmail.com>
2023-07-31 12:01:33 +0200

    Prune code and tidy up.
    Changelog:
    - removed unused imports
    - removed unused variables
    - removed duplicated lines of code
    - reorganized the script's flow
    - changed parameters and calls to functions according to new
    implementations

--- train_MoCoDAD.py
+++ train_MoCoDAD.py
@@ -1,25 +1,24 @@
 import argparse
 import os
+import random
 
+import numpy as np
 import pytorch_lightning as pl
+import torch
 import yaml
-from pytorch_lightning.callbacks import ModelCheckpoint,LearningRateMonitor
+from models.diffusion_STS import MoCoDAD
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.strategies import DDPStrategy
-
-from utils.argparser import init_sub_args
+from utils.argparser import init_args
 from utils.dataset import get_dataset_and_loader
 from utils.ema import EMACallback
-import torch 
-import numpy as np
-import random
-from models.diffusion_STS import LitAutoEncoder as Litmodel
 
 
 
 if __name__== '__main__':
 
-    
+    # Parse command line arguments and load config file
     parser = argparse.ArgumentParser(description='Pose_AD_Experiment')
     parser.add_argument('-c', '--config', type=str, required=True,
                         default='/your_default_config_file_path')
@@ -28,55 +27,40 @@ if __name__== '__main__':
     config_path = args.config
     args = yaml.load(open(args.config), Loader=yaml.FullLoader)
     args = argparse.Namespace(**args)
+    args = init_args(args)
+    # Save config file to ckpt_dir
+    os.system(f'cp {config_path} {os.path.join(args.ckpt_dir, "config.yaml")}')     
     
-    args, dataset_args, ae_args, res_args, opt_args = init_sub_args(args)
-    
+    # Set seeds    
     torch.manual_seed(args.seed)
     random.seed(args.seed)
     np.random.seed(args.seed) 
     pl.seed_everything(args.seed)
+
+    # Set callbacks and logger
+    callbacks = [ModelCheckpoint(dirpath=args.ckpt_dir, save_top_k=2,
+                                 monitor="validation_auc" if (args.dataset_choice == 'UBnormal' or args.validation) else 'loss',
+                                 mode="max" if (args.dataset_choice == 'UBnormal' or args.validation) else 'min')]
     
-    exp_dir = os.path.join(args.exp_dir, args.dataset_choice, args.dir_name)
+    callbacks += [EMACallback()] if args.use_ema else []
     
-    os.system(f'cp {config_path} {os.path.join(exp_dir, "config.yaml")}')     
-
-    project_name = "Diffusion_" + args.project_name
-
-   
-    # Pass arguments as dataset arguments for PoseDatasetRobust
-    dataset_args.exp_dir = exp_dir
-    
-
-    model = Litmodel(args)
-
-
-    checkpoint_callback = ModelCheckpoint(dirpath=args.ckpt_dir, save_top_k=2,
-                                          monitor="validation_auc" if (dataset_args.choice == 'UBnormal' or args.validation) else 'loss',
-                                          mode="max" if (dataset_args.choice == 'UBnormal' or args.validation) else 'min'
-                                         )
-
-    lr_monitor = LearningRateMonitor(logging_interval='step')
-    print('ema = ',args.use_ema)
     if args.use_wandb:
-        wandb_logger = WandbLogger(project=args.project_name, group=args.group_name, entity=args.wandb_entity, name=args.dir_name, config=args.__dict__,log_model='all')
-        trainer = pl.Trainer.from_argparse_args(args, default_root_dir=args.ckpt_dir, 
-                        logger = wandb_logger, log_every_n_steps=20, max_epochs=args.ae_epochs,
-                        callbacks=[checkpoint_callback,lr_monitor, EMACallback()] if args.use_ema else [checkpoint_callback,lr_monitor],
-                        val_check_interval=0.5, num_sanity_val_steps=0, 
-                        strategy = DDPStrategy(find_unused_parameters=True), deterministic=True)
+        callbacks += [LearningRateMonitor(logging_interval='step')]
+        wandb_logger = WandbLogger(project=args.project_name, group=args.group_name, entity=args.wandb_entity, 
+                                   name=args.dir_name, config=vars(args), log_model='all')
     else:
         wandb_logger = None
-        trainer = pl.Trainer.from_argparse_args(args, default_root_dir=args.ckpt_dir, 
-                            logger = wandb_logger, log_every_n_steps=20, max_epochs=args.ae_epochs,
-                            callbacks=[checkpoint_callback,EMACallback()] if args.use_ema else [checkpoint_callback] ,
-                            val_check_interval=0.5, num_sanity_val_steps=0, 
-                            strategy = DDPStrategy(find_unused_parameters=True))
 
-    if args.validation:
-        train_dataset, train_loader, val_dataset, val_loader = get_dataset_and_loader(dataset_args, split=args.split, validation=args.validation)
-        
-        trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
-    else:
-        train_dataset, train_loader = get_dataset_and_loader(dataset_args, split=args.split)
-        trainer.fit(model=model, train_dataloaders=train_loader)
+    # Get dataset and loaders
+    _, train_loader, _, val_loader = get_dataset_and_loader(args, split=args.split, validation=args.validation)
     
+    # Initialize model and trainer
+    model = MoCoDAD(args)
+        
+    trainer = pl.Trainer.from_argparse_args(args, default_root_dir=args.ckpt_dir, 
+                        logger=wandb_logger, log_every_n_steps=20, max_epochs=args.n_epochs,
+                        callbacks=callbacks, val_check_interval=1., num_sanity_val_steps=0, 
+                        strategy=DDPStrategy(find_unused_parameters=False), deterministic=True)
+    
+    # Train the model    
+    trainer.fit(model=model, train_dataloaders=train_loader, val_dataloaders=val_loader)
