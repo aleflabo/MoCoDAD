37f17ed9ff3563934aee5bc16a1b595c6497c1b3
stdrr <development.stdrr@gmail.com>
2023-08-13 11:50:31 +0200

    Patch argument parsing and environment.yaml

--- utils/argparser.py
+++ utils/argparser.py
@@ -17,8 +17,7 @@ def init_args(args):
                 args.pose_path['validation'] = os.path.join(args.data_dir, 'pose', 'validating/tracked_person/')
                 args.gt_path = os.path.join(args.data_dir, 'validating', 'test_frame_mask')
             else:
-                args.pose_path['validation'] = os.path.join(args.data_dir, 'pose', 'testing/tracked_person/')
-                args.gt_path = os.path.join(args.data_dir, 'testing', 'test_frame_mask')
+                raise ValueError('Validation is not available for this dataset: {}'.format(args.dataset_choice))
             
     elif args.dataset_choice == 'Avenue':
         args.pose_path = {
@@ -27,7 +26,6 @@ def init_args(args):
         print('Not usable yet.')
         exit(-1)
 
-    # if args.is_train:
     args.ckpt_dir = create_experiment_dirs(args)
 
     return args
@@ -37,11 +35,13 @@ def create_experiment_dirs(args):
     dataset = args.dataset_choice
     checkpoints_dir = os.path.join(args.exp_dir, dataset, args.dir_name)
     dirs = [checkpoints_dir]
-    try:
-        for dir_ in dirs:
-            os.makedirs(dir_, exist_ok=True)
-        print("Experiment directories created in {}".format(checkpoints_dir))
-        return checkpoints_dir
-    except Exception as err:
-        print("Experiment directories creation Failed, error {}".format(err))
-        exit(-1)
+    if args.create_experiment_dir: 
+        try:
+            for dir_ in dirs:
+                os.makedirs(dir_, exist_ok=True)
+            print("Experiment directories created in {}".format(checkpoints_dir))
+        except Exception as err:
+            print("Experiment directories creation Failed, error {}".format(err))
+            exit(-1)
+    return checkpoints_dir
+    
